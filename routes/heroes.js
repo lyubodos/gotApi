b7365d83966dcdee9ea1f5d622ce1d684aae5522
Lyubo <lyubomir.vasilev1992u@gmail.com>
2021-10-28 18:44:38 +0300

    Add routing path for editing
    Added a routing path function from with a POST request method.
    
    Patch and update methods will be imnplemented at a later stage of the
    project.

--- routes/heroes.js
+++ routes/heroes.js
@@ -6,6 +6,7 @@ const heroController = require("../controllers/heroControllers");
 router.get("/", heroController.getHeroes); 
 
 router.post("/add", heroController.addHero);
+router.post("/edit/:id", heroController.editHero);
 
 router.delete("/:id", heroController.deleteHero);
 
