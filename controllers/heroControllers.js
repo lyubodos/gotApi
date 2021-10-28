52b6b0ce47bad2e9b5600b291e9de67fcfe16692
Lyubo <lyubomir.vasilev1992u@gmail.com>
2021-10-28 18:43:19 +0300

    Add controller function for edit functionality
    Added a controller function for editing an  already created hero. The
    current edit function is called from a POST request.

--- controllers/heroControllers.js
+++ controllers/heroControllers.js
@@ -42,11 +42,38 @@ const addHero = (req, res, next) => {
         .catch(err => res.status(400).json(err));
 };
 
+const editHero = (req, res, next) => {
+    const exId = req.params.id;
+
+    const heroName = req.body.name;
+    const heroAge = req.body.age;
+    const heroHouse = req.body.house;
+    const heroStrength = req.body.strength;
+    const heroStamina = req.body.stamina;
+    const heroAgility = req.body.agility;
+    const heroIntellect = req.body.intellect;
+
+    Hero.findById(exId)
+        .then(hero => {
+            hero.name = heroName;
+            hero.age = heroAge;
+            hero.house = heroHouse;
+            hero.strength = heroStrength;
+            hero.stamina = heroStamina;
+            hero.agility = heroAgility;
+            hero.intellect= heroIntellect;
+
+            hero.save()
+            .then(result => res.json(result))
+        })
+        .catch(err => res.status(400).json(err));
+}
 
 const deleteHero = (req, res, next) => {
     const exId = req.params.id;
+    
 
-    Hero.findByIdAndDelete(exId)
+    Hero.findById(exId)
         .then(result => {
             res.json(result);
             console.log(`${result.name} has been deleted!`);
@@ -57,5 +84,6 @@ const deleteHero = (req, res, next) => {
 module.exports = {
     getHeroes,
     addHero,
+    editHero,
     deleteHero
 }
