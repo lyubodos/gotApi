3625f2de97e19321ddffc873af867ce5b04e51f8
Lyubo <lyubomir.vasilev1992u@gmail.com>
2021-11-19 00:49:02 +0200

    Add a route for retrieving a single hero
    Added a new route function in the router directory, for fetching a
    single hero data from the DB.
    
    Implementation needed for basic MVC functionality.

--- routes/heroes.js
+++ routes/heroes.js
@@ -4,12 +4,12 @@ const heroController = require("../controllers/heroControllers");
 
 
 router.get("/", heroController.getHeroes); 
+router.get("/:id", heroController.getHero);
 
 router.post("/add", heroController.addHero);
 router.post("/edit/:id", heroController.editHero);
 
 router.delete("/:id", heroController.deleteHero);
-
 
 
 
